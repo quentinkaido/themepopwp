95a9f9a6cb7ed0e5ac70073f553135d33a9a2683
Quentin Leroux <quentin.leroux@hotmail.com>
2017-09-12 18:51:11 +0200

    version 0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# wordpressproject
+
+Version 0.2.1
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="wp-content/themes/popschool/style.css">
+	<title>CV Leroux Quentin</title>
+</head>
+
+<body>
+	<!-- Première page -->
+		<div id="txt_header">
+			<br><br><br>
+      <img class="logopop" src="wp-content/themes/popschool/assets/img/pop.svg" alt="logo">
+			<h1>PopSchool</h1>
+			<h4>Site en construction, revenez plus tard !</h4>
+			</div>
+
+<div class="firstscroll">
+
+
+    <h1 class="titlehomepage">Titre 1</h1>
+
+    </div>
+
+
+
+<footer id="footer_base">
+
+</footer>
+
+	</body>
+	</html>
