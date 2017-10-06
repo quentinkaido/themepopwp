f9ff698ba9484cff9ead2828bba74fa8ca0bde8c
Quentin Leroux <quentin.leroux@hotmail.com>
2017-10-06 10:02:09 +0200

    ajout footer, logo, lien

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
 <div class="footer">
 
+
+<div class="logofooter">
+
+<a href="https://www.facebook.com/POPSchoolHDF/"><img class="logofooter1"src="wp-content/themes/popschool/front/assets/img/iconmonstr-facebook-4-72.png" alt="logo"></a>
+<a href="https://twitter.com/plus2popschool?lang=fr"><img class="logofooter1" src="wp-content/themes/popschool/front/assets/img/iconmonstr-twitter-4-72.png" alt="logo"></a>
+
+<div class="copyright">
+  Tout droit réservé à Popschool, 2017.
+</div>
+</div>
 </div>
